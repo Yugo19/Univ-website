088a5116ede345141fcf848223cde42c346a2639
Yugo19 <64170643+Yugo19@users.noreply.github.com>
2021-03-19 15:00:38 +0000

    Teachers section added

---
+++ Teachers.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Teachers</title>
+</head>
+<body>
+    <div class="classroom">
+        <div class="IRT2">
+            <img src="online.ico" alt="">
+        </div>
+    </div>
+</body>
+</html>
